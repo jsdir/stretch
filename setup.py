20ca83af687122215c70af625d4ff805dad0ab16
Jason Sommer <gatoralli69@gmail.com>
2013-09-05 13:25:21 -0500

    Changed version source

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@
 import os
 from setuptools import setup, find_packages
 
+from stretch import __version__
+
 with open('requirements.txt') as f:
     install_requires = f.read().splitlines()
 
 setup(
     name='stretch',
-    version='0.1.0',
+    version=__version__,
     description="A private PaaS.",
     long_description=open('README.md').read(),
     packages=find_packages(),
