d3900d8f6506adf3edca1ff7e2cb6524b0bef772
Jason Sommer <gatoralli69@gmail.com>
2013-10-02 00:01:16 -0500

    Added coverage command in fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,14 @@
 import os
-from fabric.api import local
+from fabric.api import local, hide
 
 local_dir = os.path.dirname(os.path.realpath(__file__))
 
+
 def test():
-    local('%s test --rednose' % os.path.join(local_dir, 'manage.py'))
+    with hide('status', 'aborts'):
+        local('%s test --rednose --nologcapture' % os.path.join(local_dir, 'manage.py'))
+
+
+def cov():
+    with hide('status', 'aborts'):
+        local('%s test --rednose --nologcapture --with-coverage --cover-package=stretch' % os.path.join(local_dir, 'manage.py'))
