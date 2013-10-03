0db35c2f398ea8bcdb9e1935ed43e3ccd1fcdc83
Jason Sommer <gatoralli69@gmail.com>
2013-10-03 04:45:11 -0500

    Changed source tests

--- tests/stretch/test_sources.py
+++ tests/stretch/test_sources.py
@@ -2,16 +2,39 @@ from mock import Mock, patch, call
 from nose.tools import eq_, raises
 import time
 
-from stretch import sources, signals
+from stretch import sources
+
+
+class TestSource(object):
+    @raises(NameError)
+    def test_require_option(self):
+        source = sources.Source({'foo': 'bar'})
+        eq_(source.require_option('foo'), 'bar')
+        option = source.require_option('what')
+
+
+class TestAutoloadableSource(object):
+    def test_autoload_is_default(self):
+        source = sources.AutoloadableSource({})
+        eq_(source.autoload, True)
+
+    def test_watch_fails_without_autoload(self):
+        source = sources.AutoloadableSource({'autoload': False})
+        source.do_watch = Mock()
+        source.watch()
+        assert not source.do_watch.called
+
+    def test_watch_runs_with_autoload(self):
+        source = sources.AutoloadableSource({'autoload': True})
+        source.do_watch = Mock()
+        source.watch()
+        assert source.do_watch.was_called
 
 
 class TestFileSystemSource(object):
-    @patch('stretch.sources.FileSystemSource.parse')
-    def test_event_handling(self, parse):
-        source = sources.FileSystemSource({'path': 'foo'})
-        source.on_files_change = Mock()
-
-        handler = sources.EventHandler(source)
+    def test_event_handler(self):
+        callback = Mock()
+        handler = sources.EventHandler(callback)
         timeout = handler.timeout = 0.1
 
         handler.on_any_event(1)
@@ -21,64 +44,4 @@ class TestFileSystemSource(object):
         handler.on_any_event(1)
         time.sleep(0.15)
 
-        eq_(source.on_files_change.mock_calls, [call([1, 2]), call([1])])
-
-    @patch('stretch.sources.FileSystemSource.parse')
-    def test_get_path(self, parse):
-        source = sources.FileSystemSource({'path': 'foo'})
-        eq_(source.get_path(), 'foo')
-
-    @raises(NameError)
-    def test_fails_without_path(self):
-        source = sources.FileSystemSource({})
-
-    """
-    @patch('stretch.sources.FileSystemSource.parse')
-    def test_sends_source_changed_signal(self):
-        source = sources.FileSystemSource({'path': 'foo'})
-        source.on_files_change(['foo', 'bar'])
-    """
-
-
-class TestGitRepositorySource(object):
-    @patch('stretch.sources.GitRepositorySource.pull')
-    def test_get_path(self, pull):
-        source = sources.GitRepositorySource({
-            'ref': '00000000',
-            'url': 'url'
-        })
-        source.get_path()
-        assert pull.was_called
-
-    @raises(NameError)
-    def test_fails_without_url(self):
-        source = sources.GitRepositorySource({'ref': '00000000'})
-
-
-class TestSource(object):
-    @patch('stretch.sources.SourceParser')
-    def test_parser_change(self, source_parser):
-        source = sources.Source({})
-        eq_(source.parser, None)
-        parser = source.parse()
-        source.parse()
-        eq_(parser, source.existing_parser)
-
-
-class TestAutoloadableSource(object):
-    def test_autoload_is_default(self):
-        source = sources.AutoloadableSource({})
-        eq_(source.autoload, True)
-
-    @raises(Exception)
-    def test_monitor_fails_without_autoload(self):
-        source = sources.AutoloadableSource({'autoload': False})
-        source.do_monitor = Mock()
-        source.monitor()
-        assert not source.do_monitor.called
-
-    def test_monitor_runs_with_autoload(self):
-        source = sources.AutoloadableSource({'autoload': True})
-        source.do_monitor = Mock()
-        source.monitor()
-        assert source.do_monitor.was_called
+        eq_(callback.mock_calls, [call([1, 2]), call([1])])
