3440cc4225c7e6a45f893c6f3acfe692069efa51
Jason Sommer <gatoralli69@gmail.com>
2013-10-25 00:52:58 -0500

    Refactored system tests

--- tests/stretch/models/test_system.py
+++ tests/stretch/models/test_system.py
@@ -1,19 +1,23 @@
 from mock import Mock, patch
 from nose.tools import eq_, raises
+from testtools import TestCase
 
 from stretch import models, exceptions
 
 
-class TestSystem(object):
+class TestSystem(TestCase):
+    def setUp(self):
+        super(TestSystem, self).setUp()
+        self.system = models.System(name='system')
+
     @raises(exceptions.UndefinedSource)
     @patch('stretch.sources.get_sources')
     def test_source(self, get_sources):
         get_sources.return_value = ['foo']
-        system = models.System(name='system')
-        eq_(system.source, 'foo')
+        eq_(self.system.source, 'foo')
         get_sources.return_value = []
-        system = models.System(name='bar')
-        system.source
+        self.system = models.System(name='bar')
+        self.system.source
 
     @patch('stretch.models.System.environments')
     @patch('stretch.models.System.source')
@@ -23,19 +27,18 @@ class TestSystem(object):
         env = Mock()
         env.backend.autoloads = True
         environments.all.return_value = [env]
-        system = models.System(name='system')
-        system.sync_source()
+        self.system.sync_source()
         env.deploy.delay.assert_called_with(source)
 
         env.backend.autoloads = False
         env.deploy.delay.reset_mock()
-        system.sync_source()
+        self.system.sync_source()
         assert not env.deploy.delay.called
 
         source.autoload = False
         env.backend.autoloads = True
         env.deploy.delay.reset_mock()
-        system.sync_source()
+        self.system.sync_source()
         assert not env.deploy.delay.called
 
     @patch('stretch.models.System.environments')
@@ -46,17 +49,24 @@ class TestSystem(object):
         env = Mock()
         env.backend.autoloads = True
         environments.all.return_value = [env]
-        system = models.System(name='system')
-        system.sync_source(['foo', 'bar'])
+        self.system.sync_source(['foo', 'bar'])
         env.autoload.delay.assert_called_with(source, ['foo', 'bar'])
 
         env.backend.autoloads = False
         env.autoload.delay.reset_mock()
-        system.sync_source(['foo', 'bar'])
+        self.system.sync_source(['foo', 'bar'])
         assert not env.autoload.delay.called
 
         source.autoload = False
         env.backend.autoloads = True
         env.autoload.delay.reset_mock()
-        system.sync_source(['foo', 'bar'])
+        self.system.sync_source(['foo', 'bar'])
         assert not env.autoload.delay.called
+
+    @patch('stretch.models.System.source')
+    @patch('stretch.models.Release')
+    def test_create_release(self, release_mock, source):
+        source.pull = Mock(return_value='path')
+        release = self.system.create_release({'key': 'value'})
+        release_mock.create.assert_called_with('path', system=self.system)
+        self.system.source.pull.assert_called_with({'key': 'value'})
