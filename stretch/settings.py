86338fcfd98bfee0f2b6c9fe3e8c9fb359c3aaf4
Jason Sommer <gatoralli69@gmail.com>
2014-01-22 13:52:44 -0600

    Added nose test runner

--- stretch/settings.py
+++ stretch/settings.py
@@ -121,12 +121,16 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'django_nose',
+
     'stretch',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
