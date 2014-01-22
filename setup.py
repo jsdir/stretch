afa6feea5fb5f58cf1a9256e1b4d3917248097eb
Jason Sommer <gatoralli69@gmail.com>
2014-01-22 13:06:45 -0600

    Bumped Django version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ tests_require = [
 ]
 
 install_requires = [
-    'Django>=1.5.5,<1.6',
+    'Django>=1.6.1,<1.7',
     'django-celery>=3.0.11,<3.1.0',
     'Celery==3.1',
     'gunicorn>=18.0,<19.0',
